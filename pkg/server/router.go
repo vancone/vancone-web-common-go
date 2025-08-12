12d3b1a3cb014a1f1c81d37b2578c755d49006dc
Tenton Lien <tentonlien@users.noreply.github.com>
2025-08-12 17:53:33 +0800

    Add StartServer function for router

--- pkg/server/router.go
+++ pkg/server/router.go
@@ -1,7 +1,21 @@
 package server
 
-import "github.com/gin-gonic/gin"
+import (
+	"fmt"
+	"log"
+
+	"github.com/gin-gonic/gin"
+	"github.com/vancone/vancone-web-common-go/pkg/config"
+)
 
 type Router interface {
 	Load(engine *gin.Engine)
+}
+
+func StartServer(g *gin.Engine) {
+	log.Println("Server started, listening on port", config.Server.Port)
+	err := g.Run(fmt.Sprintf(":%d", config.Server.Port))
+	if err != nil {
+		log.Panicln("Failed to start server:", err)
+	}
 }
